ee9fbf4022b7bdc2a85de534f2b18073cfef8a6f
izacpeterson <izacpeterson@gmail.com>
2023-06-02 04:09:09 -0600

    env var name

--- src/lib/aws.js
+++ src/lib/aws.js
@@ -7,8 +7,8 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 export const client = new S3Client({
   region: "us-east-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_KEY,
   },
 });
 
